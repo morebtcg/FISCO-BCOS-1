267357dcc365d98c2c39ee17483e7dfb9e745743
莫楠 <651932351@qq.com>
2024-02-20 13:19:24 +0800

    Update thread count to 24

--- bcos-front/bcos-front/FrontService.cpp
+++ bcos-front/bcos-front/FrontService.cpp
@@ -34,7 +34,7 @@ using namespace front;
 using namespace protocol;
 
 FrontService::FrontService()
-  : m_taskArena(16), m_localProtocol(g_BCOSConfig.protocolInfo(ProtocolModuleID::NodeService))
+  : m_taskArena(24), m_localProtocol(g_BCOSConfig.protocolInfo(ProtocolModuleID::NodeService))
 {
     FRONT_LOG(INFO) << LOG_DESC("FrontService") << LOG_KV("this", this)
                     << LOG_KV("minVersion", m_localProtocol->minVersion())
